817af104d754b4e9c356764143280ff454beeed2
Natan Lima <natanjonatan3103@gmail.com>
2026-06-11 18:35:25 -0300

    feat(curitiba): configure playwright and map protocol selectors

--- robots/curitiba/selectors.py
+++ robots/curitiba/selectors.py
@@ -1,1 +1,8 @@
 URL_CURITIBA = "http://consultaprotocolo.curitiba.pr.gov.br/index.aspx"
+
+CAMPO_TIPO_PROTOCOLO = "#txtTipoProtocolo"
+CAMPO_NUMERO_PROTOCOLO = "#txtNumProtocolo"
+CAMPO_ANO_PROTOCOLO = "#txtAnoProtocolo"
+
+BOTAO_PESQUISAR = "input[value='Pesquisar']"
+BOTAO_LIMPAR = "input[value='Limpar']"
